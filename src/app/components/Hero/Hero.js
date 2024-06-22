d95f91de487ec9b74f116e67efc09db71f6b086c
adrsha <adrsha@github.com>
2024-06-22 20:16:01 +0545

    feat:Hero Page static version

--- src/app/components/Hero/Hero.js
+++ src/app/components/Hero/Hero.js
@@ -3,9 +3,13 @@ export default function Hero() {
   return (
     <div className="hero">
       <div className="hero-content">
-        <h1>Find the best </h1>
+        <h1>
+          Find the <i>Best Insurance</i> plans for your needs
+        </h1>
+        <button>Compare Now</button>
       </div>
       <div className="hero-advert">
+        <div className="hero-carousel"></div>
         <span className="hero-carousel-index">
           <li></li>
           <li></li>
