8665fda18a66f2234bdf63f9e5e57cf63aa22b52
adrsha <adrsha@github.com>
2024-06-22 16:43:42 +0545

    feat:colors added in globals.css

---
+++ src/app/components/Hero/Hero.js
@@ -0,0 +1,17 @@
+import "./Hero.css";
+export default function Hero() {
+  return (
+    <div className="hero">
+      <div className="hero-content">
+        <h1>Find the best </h1>
+      </div>
+      <div className="hero-advert">
+        <span className="hero-carousel-index">
+          <li></li>
+          <li></li>
+          <li></li>
+        </span>
+      </div>
+    </div>
+  );
+}
