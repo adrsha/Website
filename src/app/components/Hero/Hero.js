5aab8aca6be0ae87d3b58b2a5a87243f8612958c
adrsha <adrsha@github.com>
2024-06-23 16:52:33 +0545

    feat: Hero's dynamic carousel'

--- src/app/components/Hero/Hero.js
+++ src/app/components/Hero/Hero.js
@@ -1,47 +1,49 @@
-import Image from "next/image";
+"use client";
+import "mathjs";
+import HeroCarousel from "./HeroCarouselImg.js";
 import "./Hero.css";
+import { useState } from "react";
+
+const carouselIndex = new Array(3).fill(0).map((_, i) => i);
 
 export default function Hero() {
-  const imageLoader = (width, height) => {
-    return `https://picsum.photos/${width}/${height}`;
-  };
-  const carouselSizeWidth = 1920 * 0.2;
-  const carouselSizeHeight = 1080 * 0.4;
-
+  const [pos, setPos] = useState(0);
+  const carouselIndexEl = carouselIndex.map((i) => {
+    return (
+      <span
+        id={`carouselIndex` + i}
+        key={i}
+        className={
+          "heroCarouselIndexBalls " + (i === pos ? "activeCarouselIndex" : "")
+        }
+        onClick={(e) => {
+          setPos(i);
+          Array.from(e.target.parentElement.children).forEach((e_ch) => {
+            e_ch.classList.remove("activeCarouselIndex");
+          });
+          e.target.classList.add("activeCarouselIndex");
+        }}
+      ></span>
+    );
+  });
   return (
     <div className="hero">
-      <div className="hero-content">
+      <div className="heroContent">
         <h1>
           Find the <i>Best Insurance</i> plans for your needs
         </h1>
         <button>Compare Now</button>
       </div>
-      <div className="hero-advert">
-        <div className="hero-carousel">
-          <div className="hero-carousel-item">
-            <Image
-              src={imageLoader(carouselSizeWidth, carouselSizeHeight)}
-              alt="advert_1"
-              width={carouselSizeWidth}
-              height={carouselSizeHeight}
-            />
-          </div>
-          <div className="hero-carousel-item">
-            <Image
-              src={imageLoader(carouselSizeWidth, carouselSizeHeight)}
-              alt="advert_2"
-              width={carouselSizeWidth}
-              height={carouselSizeHeight}
-            />
-          </div>
+      <div className="heroAdvert">
+        <div className="heroCarousel">
+          <HeroCarousel pos={0 - pos} />
+          <HeroCarousel pos={1 - pos} />
+          <HeroCarousel pos={2 - pos} />
+          <span id="heroAdventPrev">←</span>
+          <span id="heroAdventNext">→</span>
         </div>
-        <span className="hero-carousel-index">
-          <li></li>
-          <li></li>
-          <li></li>
-        </span>
+        <span className="heroCarouselIndex">{carouselIndexEl}</span>
       </div>
     </div>
   );
 }
-
