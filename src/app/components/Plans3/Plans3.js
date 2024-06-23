1072329213c074a7b39699821a69948c04fcff9a
paudelgamer <paudeladrin@gmail.com>
2024-06-23 16:28:04 +0545

    feat: added 3 plans display in home page

---
+++ src/app/components/Plans3/Plans3.js
@@ -0,0 +1,73 @@
+
+import "./Plans3.css";
+export default function Plans3() {
+    return (
+        <div className="plans">
+            <div className="heading">
+                Available Plans
+            </div>
+
+            <div className="allThePlansContainer">
+
+                <div className="plan">
+                    <div className="planContent" >
+                        <div className="planHeader">Endowment </div>
+                        <div className="planText">
+                            <p>
+                                This plan combines life insurance with a savings component,
+                                providing a lump sum payout either upon the policyholder's death
+                                or at the end of the policy term, whichever comes first.
+                            </p>
+                        </div>
+                    </div>
+
+                    <div className="imageContainer">
+                        <div className="imageExtension">Most Selected</div>
+                        <div className="planImg" id="endowmentphoto"></div>
+                    </div>
+
+                </div>
+
+                <div className="plan">
+                    <div className="planContent" >
+                        <div className="planHeader">Term Life</div>
+                        <div className="planText">
+                            <p>
+                                This plan provides pure life insurance coverage for a specified
+                                duration, offering a death benefit only if the insured person
+                                passes away during the term of the policy.
+                            </p>
+                        </div>
+                    </div>
+
+                    <div className="imageContainer">
+                        <div className="imageExtension">Early payout</div>
+                        <div className="planImg" id="termlifephoto"></div>
+                    </div>
+
+                </div>
+
+                <div className="plan">
+                    <div className="planContent" >
+                        <div className="planHeader">Money Back</div>
+                        <div className="planText">
+                            <p>
+                                This plan is a life insurance policy that returns a portion of
+                                the sum assured at regular intervals during the policy term,
+                                along with a lump sum payout at maturity or upon the
+                                policyholder's death.
+                            </p>
+                        </div>
+                    </div>
+
+                    <div className="imageContainer">
+                        <div className="imageExtension">Early payout</div>
+                        <div className="planImg" id="moneybackphoto" ></div>
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+    );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import Hero from "./components/Hero/Hero";
+import Plans3 from "./components/Plans3/Plans3";
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <Plans3 />
     </>
   );
 }
