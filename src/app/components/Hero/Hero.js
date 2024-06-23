4688cedcc2e24f1b8416c2c66adb4e9a77b708a0
adrsha <adrsha@github.com>
2024-06-23 09:51:58 +0545

    fix: push not detected

--- src/app/components/Hero/Hero.js
+++ src/app/components/Hero/Hero.js
@@ -1,5 +1,17 @@
+import Image from "next/image";
 import "./Hero.css";
+import { viewportWidth, viewportHeight } from "./CarouselImages";
+
 export default function Hero() {
+  const imageLoader = (width, height) => {
+    return `https://picsum.photos/${width}/${height}`;
+  };
+  console.log(viewportWidth, viewportHeight);
+  // const carouselSizeWidth = viewportWidth * 0.2;
+  // const carouselSizeHeight = viewportHeight * 0.4;
+  const carouselSizeWidth = 1920 * 0.2;
+  const carouselSizeHeight = 1080 * 0.4;
+
   return (
     <div className="hero">
       <div className="hero-content">
@@ -9,7 +21,24 @@ export default function Hero() {
         <button>Compare Now</button>
       </div>
       <div className="hero-advert">
-        <div className="hero-carousel"></div>
+        <div className="hero-carousel">
+          <div className="hero-carousel-item">
+            <Image
+              src={imageLoader(carouselSizeWidth, carouselSizeHeight)}
+              alt="advert_1"
+              width={carouselSizeWidth}
+              height={carouselSizeHeight}
+            />
+          </div>
+          <div className="hero-carousel-item">
+            <Image
+              src={imageLoader(carouselSizeWidth, carouselSizeHeight)}
+              alt="advert_2"
+              width={carouselSizeWidth}
+              height={carouselSizeHeight}
+            />
+          </div>
+        </div>
         <span className="hero-carousel-index">
           <li></li>
           <li></li>
