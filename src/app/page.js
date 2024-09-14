8d8efaf8eec47d0499d6cfaf2dde600cca67854e
adrsha <adrsha@github.com>
2024-09-14 13:09:02 +0545

    Reapply "feat: added compare page"
    This reverts commit 32b2035c6a5942795e6983116d5fa3135895f49c.

--- src/app/page.js
+++ src/app/page.js
@@ -4,15 +4,12 @@ import Plans3 from "./components/Plans3/Plans3";
 import Reviews from "./components/Reviews/Reviews";
 import Faq from "./components/Faq/Faq";
 import Why from "./components/Why/Why";
+import Compare from "./compareNow/Compare";
 
 export default function Home() {
   return (
     <>
-      <Hero />
-      <Plans3 />
-      <Why />
-      <Reviews />
-      <Faq />
+      <Compare />
     </>
   );
 }
